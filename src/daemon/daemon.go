3905f8f9428ef2b286bf4c084a8acf8521f47d7b
Tian23 <tian23@illinois.edu>
2022-09-22 22:01:24 -0500

    refactor handle method, use producer/cosumer model

---
+++ src/daemon/daemon.go
@@ -0,0 +1,296 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"mp2-hangy6-tian23/src/utils"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type member struct {
+	ip string
+	id string
+}
+
+type list struct {
+	members []member
+}
+
+/*******  Global Variable ******/
+
+// const introducer = "127.0.0.1"
+
+var localIp = utils.GetLocalIP()
+var localID = utils.GenerateID(localIp)
+var port = 9980
+var memList list
+var monList list
+var operaChan = make(chan string, 1024)
+var responChan = make(chan list, 1024)
+var killChan = make(chan string, 1024)
+var listChan = make(chan list, 1024)
+
+/*******************************/
+
+// delete failed or leaved node from local list
+func del(target string) {
+	// fmt.Println("------------------")
+	// fmt.Println("Cur members:", memList.members)
+	// fmt.Println("kill member:", target)
+	// kill cur monitors
+	operaChan <- "KILL"
+
+	if strings.Compare(target, localID) == 0 {
+		// do not delete self
+		return
+	}
+	var idx int = -1
+	for i, m := range memList.members {
+		if strings.Compare(m.id, target) == 0 {
+			idx = i
+			break
+		}
+	}
+	if idx == -1 {
+		// do not need to send message now
+		operaChan <- "RESTART"
+		// fmt.Println("After members:", memList.members)
+		// fmt.Println("------------------")
+		return
+	}
+	memList.members = append(memList.members[:idx], memList.members[idx+1:]...)
+
+	idx = -1
+	for i, m := range memList.members {
+		if strings.Compare(m.id, localID) == 0 {
+			idx = i
+			break
+		}
+	}
+	monList.members = []member{}
+	// if we do not have at least 3 other members
+	if len(memList.members) <= 3 {
+		monList.members = append(monList.members, memList.members[:idx]...)
+		monList.members = append(monList.members, memList.members[idx+1:]...)
+	} else {
+		var newList []member
+		for i := 1; i <= 3; i++ {
+			newList = append(newList, memList.members[(idx+i)%len(memList.members)])
+		}
+		monList.members = newList
+	}
+	operaChan <- "RESTART"
+	// fmt.Println("After members:", memList.members)
+	// fmt.Println("------------------")
+	return
+}
+
+// handle operations
+func operationsBank() {
+	for {
+		operation := <-operaChan
+		// fmt.Println(operation)
+		id := operation[3:]
+		if strings.Compare(operation[:3], "DEL") == 0 {
+			del(id)
+		} else if strings.Compare(operation[:3], "ADD") == 0 {
+			//
+		} else if strings.Compare(operation[:3], "MON") == 0 {
+			responChan <- monList
+		} else if strings.Compare(operation[:4], "READ") == 0 {
+			listChan <- memList
+		} else if strings.Compare(operation[:4], "KILL") == 0 {
+			for i := 0; i < len(monList.members); i++ {
+				killChan <- "KILL"
+			}
+		} else if strings.Compare(operation[:7], "RESTART") == 0 {
+			startMonitor()
+		}
+	}
+}
+
+func handleFailOrLeaveMsg(m utils.Message) {
+	// delete failed node
+	operaChan <- "DEL" + m.Payload
+	failMsg := utils.Msg2Json(utils.CreateMsg(localIp, localID, utils.FAIL, m.Payload))
+	operaChan <- "MON"
+	curMon := <-responChan
+	// send fail message to others
+	for _, mem := range curMon.members {
+		dstAddr := &net.UDPAddr{IP: net.ParseIP(mem.ip), Port: port}
+		// build connection
+		conn, err := net.DialUDP("udp", nil, dstAddr)
+		if err != nil {
+			log.Fatal("Something wrong when build udp conn with ", mem.id)
+		}
+		// send message
+		_, err = conn.Write(failMsg)
+		if err != nil {
+			log.Fatal("Something wrong when send udp packet to", mem.id)
+		}
+	}
+}
+
+// start to monitor
+func startMonitor() {
+	for _, mon := range monList.members {
+		go monitor(mon)
+	}
+}
+
+// ping all peers in monitor list every 2.5 seconds
+func monitor(mon member) {
+	var pingMsg utils.Message = utils.CreateMsg(localIp, localID, utils.PING, "")
+	msg := utils.Msg2Json(pingMsg)
+	var rcvMsg = make([]byte, 1024)
+	// set a ticker to send ping message periodically
+	ticker := time.NewTicker(2500 * time.Millisecond)
+	defer ticker.Stop()
+	for {
+		flag := true
+		select {
+		case <-ticker.C:
+			dstAddr := &net.UDPAddr{IP: net.ParseIP(mon.ip), Port: port}
+			// build connection
+			conn, err := net.DialUDP("udp", nil, dstAddr)
+			if err != nil {
+				log.Fatal("Something wrong when build udp conn with ", mon.id)
+			}
+			// send message
+			_, err = conn.Write(msg)
+			if err != nil {
+				log.Fatal("Something wrong when send udp packet to", mon.id)
+			}
+			// set read deadline for timeout
+			conn.SetReadDeadline(time.Now().Add(time.Duration(2000) * time.Millisecond))
+
+			// try to get ack message from target
+			n, err := conn.Read(rcvMsg)
+			_ = utils.Json2Msg(rcvMsg[:n])
+			// fmt.Println(receive)
+			if err != nil {
+				// monitor object failed
+				ticker.Stop()
+				// flag = false
+				// delete the failed node
+				operaChan <- "DEL" + mon.id
+				failMsg := utils.Msg2Json(utils.CreateMsg(localIp, localID, utils.FAIL, mon.id))
+				// get the monitor list
+				operaChan <- "MON"
+
+				curMon := <-responChan
+				// send fail message to others
+				for _, mon := range curMon.members {
+					dstAddr := &net.UDPAddr{IP: net.ParseIP(mon.ip), Port: port}
+					// build connection
+					conn, err := net.DialUDP("udp", nil, dstAddr)
+					if err != nil {
+						log.Fatal("Something wrong when build udp conn with " + mon.id)
+					}
+					// send message
+					_, err = conn.Write(failMsg)
+					if err != nil {
+						log.Fatal("Something wrong when send udp packet to" + mon.id)
+					}
+				}
+			}
+			// monitor object is still alive
+			// fmt.Println("target is still alive", mon.id)
+			// close the connection
+			conn.Close()
+		case <-killChan:
+			flag = false
+		}
+		// if monitor peer failed, break the loop
+		if !flag {
+			// fmt.Println("Break")
+			break
+		}
+	}
+}
+
+// listen to specific port: 9980 to reply for ping
+func handler() {
+	udpAddr, err := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(port))
+	if err != nil {
+		log.Fatal("Something wrong when resolve local address")
+	}
+	// build conn
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		log.Fatal("Something wrong when listen port")
+	}
+	defer conn.Close()
+	var rcvMsg = make([]byte, 1024)
+	for {
+		n, srcAddr, err := conn.ReadFromUDP(rcvMsg)
+		if err != nil {
+			fmt.Println("Error when read udp packet")
+		}
+		msg := utils.Json2Msg(rcvMsg[:n])
+		// fmt.Println(msg)
+		// handle different types of messages
+		switch msg.Type {
+		// reply ack message when receive ping message
+		case utils.PING:
+			ackMsg := utils.CreateMsg(localIp, localID, utils.ACK, "")
+			_, err = conn.WriteToUDP(utils.Msg2Json(ackMsg), srcAddr)
+			if err != nil {
+				fmt.Println("Error when send back Ack message")
+			}
+		// delete fail node from local when receive fail message
+		case utils.FAIL:
+			go handleFailOrLeaveMsg(msg)
+		// delete leave node from local when receive leave message
+		case utils.LEAVE:
+			go handleFailOrLeaveMsg(msg)
+		}
+	}
+}
+
+func commandServer() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Plesase input the command.")
+	for {
+		text, _, _ := reader.ReadLine()
+		command := string(text)
+		if strings.Compare(command, "list_mem") == 0 {
+			operaChan <- "READ"
+			curMem := <-listChan
+			for _, mem := range curMem.members {
+				fmt.Println(mem.id)
+			}
+		} else if strings.Compare(command, "list_self") == 0 {
+			fmt.Println(localID)
+		} else if strings.Compare(command, "join") == 0 {
+			// join
+		} else if strings.Compare(command, "leave") == 0 {
+			// leave
+		} else if strings.Compare(command, "list_mon") == 0 {
+			operaChan <- "MON"
+			curMon := <-responChan
+			for _, mem := range curMon.members {
+				fmt.Println(mem.id)
+			}
+		}
+	}
+}
+
+func main() {
+	memList.members = []member{{"192.16.2.1", "test5"}, {"192.16.2.1", "test6"}, {"192.16.2.1", "test7"}, {"192.16.2.1", "test4"}, {localIp, localID}, {"192.16.2.1", "test1"}, {"192.16.2.1", "test2"}, {"192.16.2.1", "test3"}}
+	// memList.members = []member{{localIp, localID}}
+	monList.members = []member{{"192.16.2.1", "test1"}, {"192.16.2.1", "test2"}, {"192.16.2.1", "test3"}}
+	// monList.members = []member{{localIp, localID}}
+	fmt.Println(monList.members)
+	go operationsBank()
+	go startMonitor()
+	go handler()
+	go commandServer()
+	for {
+	}
+}
--- src/utils/message.go
+++ src/utils/message.go
@@ -13,12 +13,13 @@ const (
 
 type Message struct {
 	SrcIp   string `json:srcip`
+	SrcID   string `json:srcid`
 	Type    int    `json:type`
 	Payload string `json:payload`
 }
 
-func CreateMsg(ip string, msgType int, payload string) Message {
-	return Message{SrcIp: ip, Type: msgType, Payload: payload}
+func CreateMsg(ip string, id string, msgType int, payload string) Message {
+	return Message{SrcIp: ip, SrcID: id, Type: msgType, Payload: payload}
 }
 
 func Json2Msg(data []byte) Message {
